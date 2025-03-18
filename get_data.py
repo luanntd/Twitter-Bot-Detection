57e5762ff0ec6f78f432f9e208dcc7f68cceab21
Luan <ntluan2004@gmail.com>
2025-03-18 17:41:23 +0700

    add instruction to get all data

---
+++ get_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Raw data
+base_data = pd.read_json('user.json')
+
+# Extract data from public metrics column
+list_of_public_metrics = list(base_data['public_metrics'])
+public_metrics_data = pd.DataFrame(list_of_public_metrics)
+
+# Merge data
+data = pd.concat([base_data, public_metrics_data], axis=1)
+data = data.drop('public_metrics', axis=1)
+
+# Load data label
+label = pd.read_csv('label.csv')
+
+# Labeled data
+labeled_data = pd.merge(data, label, on='id')
+
+# Save to .csv file
+labeled_data.to_csv('TwiBot22_datasets.csv', index=False)
